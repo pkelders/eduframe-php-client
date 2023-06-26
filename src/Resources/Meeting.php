8e2e26115033701ffe06ce2a52f90b81926c8781
pkelders <pakelders@home.nl>
2023-06-26 13:38:54 +0200

    Added Teachers to meeting

--- src/Resources/Meeting.php
+++ src/Resources/Meeting.php
@@ -10,30 +10,38 @@ use Eduframe\Traits\Storable;
 class Meeting extends Resource {
 	use FindAll, FindOne, Storable;
 
-
-    /**
-     * @var array
-     */
-    protected $fillable = [
-        'id',
-        'name',
-        'planned_course_id',
-        'description',
-        'description_dashboard',
-        'meeting_location_id',
-        'start_date_time',
-        'end_date_time',
-        'updated_at',
-        'created_at'
-    ];
+	/**
+	 * @var array
+	 */
+	protected $fillable = [
+		'id',
+		'name',
+		'planned_course_id',
+		'description',
+		'description_dashboard',
+		'meeting_location_id',
+        'teachers',
+		'start_date_time',
+		'end_date_time',
+		'updated_at',
+		'created_at'
+	];
 
     /**
      * @var string
      */
     protected $endpoint = 'meetings';
 
-    /**
-     * @var string
-     */
-    protected $namespace = 'meeting';
+    protected $multipleNestedEntities = [
+
+        'teachers'    => [
+            'entity' => Teacher::class,
+            'type'   => self::NESTING_TYPE_ARRAY_OF_OBJECTS,
+        ]
+    ];
+
+	/**
+	 * @var string
+	 */
+	protected $namespace = 'meeting';
 }
